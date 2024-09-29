1611ace0c52a5ce68870b859efc7ed56bc84891c
Thiago Cerutti <thiagocerutti@gmail.com>
2024-09-29 20:11:23 -0300

    feat: adiciona exemplo de retorno JSON

--- index.py
+++ index.py
@@ -11,3 +11,13 @@ def hello_world():
 def hello(name):
     result = 3+4+1
     return f"<p>Hello from /hello route. 3+4={result}, {escape(name)}!</p>"
+
+@app.route("/soma/<int:num1>/<int:num2>")
+def soma(num1, num2):
+    resultado = num1 + num2
+    return {
+        "operacao": "soma",
+        "primeiro_valor": num1,
+        "segundo_valor": num2,
+        "resultado": resultado
+    }
