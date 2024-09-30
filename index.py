37bf7246dcacb65e183ae492644608640538580f
Thiago Cerutti <thiagocerutti@gmail.com>
2024-09-30 18:52:46 -0300

    feat: refactoring

--- index.py
+++ index.py
@@ -6,41 +6,40 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello, Pedro and Alice!</h1>"
 
 @app.route("/hello/<name>")
 def hello(name):
     result = 3+4+1
-    return f"<p>Hello from /hello route. 3+4={result}, {escape(name)}!</p>"
+    return f"<p>Hello from /hello route. 3+4+1={result}, {escape(name)}!</p>"
 
 @app.route("/soma/<int:num1>/<int:num2>")
 def soma(num1, num2):
-    resultado = num1 + num2
     return {
         "operacao": "soma",
         "primeiro_valor": num1,
         "segundo_valor": num2,
-        "resultado": resultado
+        "resultado": num1 + num2
     }
 
 def carregarDadosDoArquivo(path):
     dados = []
     with open(path, newline="") as csvfile:
-        reader = csv.reader(csvfile, delimiter=",")
-        next(reader)  # Skip the header row
-        for row in reader:
+        tabelaDeDados = csv.reader(csvfile, delimiter=",")
+        next(tabelaDeDados)  # Skip the header row
+        for linha in tabelaDeDados:
             dados.append({
-            "referencia": row[0],
-            "acre": row[1],
-            "amazonas": row[2],
-            "amapa": row[3],
-            "maranhao": row[4],
-            "mato_grosso": row[5],
-            "para": row[6],
-            "rondonia": row[7],
-            "roraima": row[8],
-            "tocantins": row[9],
-            "area_total_desmatamento": row[10]
+                "referencia": int(linha[0]),
+                "acre": linha[1],
+                "amazonas": linha[2],
+                "amapa": linha[3],
+                "maranhao": linha[4],
+                "mato_grosso": linha[5],
+                "para": linha[6],
+                "rondonia": linha[7],
+                "roraima": linha[8],
+                "tocantins": linha[9],
+                "area_total_desmatamento": linha[10]
             })
     return dados
 
@@ -48,12 +47,11 @@ def carregarDadosDoArquivo(path):
 def desmatamento(ano):
     tabelaDeDados = carregarDadosDoArquivo("desmatamento_prodes.csv")
     for linha in tabelaDeDados:
-        if int(linha["referencia"]) == ano:
+        if linha["referencia"] == ano:
             return linha
     return {"erro": "Não encontrado"}, 404
 
 @app.route("/desmatamento/")
 def desmatamento_total():
-    dados = carregarDadosDoArquivo("desmatamento_prodes.csv")
-    return dados
+    return carregarDadosDoArquivo("desmatamento_prodes.csv")
 
