d5ec018ae3d10256671a9d08e9702f3fc3f4c3d6
Thiago Cerutti <thiagocerutti@gmail.com>
2024-09-29 21:52:59 -0300

    feat: filtra lista

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from markupsafe import escape
+import csv
 
 app = Flask(__name__)
 
@@ -22,16 +23,37 @@ def soma(num1, num2):
         "resultado": resultado
     }
 
-def ler_arquivo(path):
-    with open(path, "r") as file:
-        return file.read()
+def carregarDadosDoArquivo(path):
+    dados = []
+    with open(path, newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        next(reader)  # Skip the header row
+        for row in reader:
+            dados.append({
+            "referencia": row[0],
+            "acre": row[1],
+            "amazonas": row[2],
+            "amapa": row[3],
+            "maranhao": row[4],
+            "mato_grosso": row[5],
+            "para": row[6],
+            "rondonia": row[7],
+            "roraima": row[8],
+            "tocantins": row[9],
+            "area_total_desmatamento": row[10]
+            })
+    return dados
 
 @app.route("/desmatamento/<int:ano>")
 def desmatamento(ano):
-    path = "desmatamento_prodes.csv"
-    dados = ler_arquivo(path)
-    return {
-        "ano": ano,
-        "dados": dados
-    }
+    dados = carregarDadosDoArquivo("desmatamento_prodes.csv")
+    for dado in dados:
+        if int(dado["referencia"]) == ano:
+            return dado
+    return {"erro": "Não encontrado"}, 404
+
+@app.route("/desmatamento/")
+def desmatamento_total():
+    dados = carregarDadosDoArquivo("desmatamento_prodes.csv")
+    return dados
 
